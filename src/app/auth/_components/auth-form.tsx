c9ba3bc3d65bad7aceafb7bc6d848a46ed9bf79f
Bhayron <bhayronklivilan@gmail.com>
2024-11-06 19:49:36 -0300

    Ajustes gerais

--- src/app/auth/_components/auth-form.tsx
+++ src/app/auth/_components/auth-form.tsx
@@ -36,7 +36,7 @@ export default function AuthForm() {
                 <CardHeader className="space-y-1">
                     <CardTitle className="text-2xl font-bold text-center">Entrar</CardTitle>
                     <CardDescription className="text-center">
-                        Bhayron no Natham
+                        Bhayron
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
